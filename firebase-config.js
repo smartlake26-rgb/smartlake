bb929135fb02203937bf4689005ea6047d50afd7
smartlake <smartlake26@gmail.com>
2026-06-25 16:24:19 +0500

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -9,13 +9,12 @@
    (Realtime Database -> sahifa tepasidagi havola).
    ============================================================ */
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA2JgIbG_kJCLSEjsU142kWyMqTBrUlTdE",
-  authDomain: "smartlake-6ce23.firebaseapp.com",
-  databaseURL: "https://smartlake-6ce23-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "smartlake-6ce23",
-  storageBucket: "smartlake-6ce23.firebasestorage.app",
-  messagingSenderId: "920951270016",
-  appId: "1:920951270016:web:0c2d2b0032149b08245465",
-  measurementId: "G-L40C0DP1GE"
+const FIREBASE_CONFIG = {
+  apiKey:            "XXXX_API_KEY_XXXX",
+  authDomain:        "smart-lake-XXXX.firebaseapp.com",
+  databaseURL:       "https://smart-lake-XXXX-default-rtdb.firebaseio.com",
+  projectId:         "smart-lake-XXXX",
+  storageBucket:     "smart-lake-XXXX.appspot.com",
+  messagingSenderId: "XXXXXXXXXXXX",
+  appId:             "1:XXXXXXXXXXXX:web:XXXXXXXXXXXXXXXX"
 };
